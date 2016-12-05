129c7c81f0d27deb12128c964690f9319dbef0c1
Mirko Cukich <mirkocukich@gmail.com>
2016-12-05 13:54:26 -0600

    Bubble Sort complete

--- src/com/digitalmirko/Main.java
+++ src/com/digitalmirko/Main.java
@@ -6,8 +6,37 @@ package com.digitalmirko;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void bubbleSort(int array[]) {
+        int n = array.length;
+        int k;
+        for (int m = n; m >= 0; m--) {
+            for (int i = 0 ; i < n - 1; i++) {
+                k = i + 1;
+                if (array[i] > array[k]) {
+                    swapNumbers(i, k, array);
+                }
+            }
+            printNumbers(array);
+        }
+    }
 
-        
+    private static void swapNumbers(int i, int j, int[] array){
+        int temp;
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    private static void printNumbers(int[] input){
+
+        for(int i = 0; i < input.length; i++){
+            System.out.print(input[i] + ", ");
+        }
+        System.out.println("\n");
+    }
+
+    public static void main(String[] args) {
+        int[] input = {6,3,7,2,8,18,45,34,16,0,21,23};
+        bubbleSort(input);
     }
 }
