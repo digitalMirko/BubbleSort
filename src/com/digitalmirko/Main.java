219ff261d3fc2b2c36fee7ee4e5106239e86dd39
Mirko Cukich <mirkocukich@gmail.com>
2016-12-05 13:37:49 -0600

    Initial commit

---
+++ src/com/digitalmirko/Main.java
@@ -0,0 +1,13 @@
+package com.digitalmirko;
+
+// Bubble Sort in Java
+// Big O Notation:
+// Best case: O(n), Average and Worst Case: O(n2)
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        
+    }
+}
