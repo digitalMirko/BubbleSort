dbaf019325df31b7f4a948568aff85cd077dfdcf
Mirko Cukich <mirkocukich@gmail.com>
2016-12-05 14:04:48 -0600

    Bubble Sort complete

--- src/com/digitalmirko/Main.java
+++ src/com/digitalmirko/Main.java
@@ -1,8 +1,8 @@
 package com.digitalmirko;
 
 // Bubble Sort Algorithm in Java
-// This algorithm steps through the given array, compares each pair of adjacent items,
-// swaps them until no other swaps are required.
+// This algorithm steps through the given array, compares each pair of
+// adjacent items, swaps them until no other swaps are required.
 // Big O Notation:
 // Best case: O(n), Average and Worst Case: O(n2)
 
